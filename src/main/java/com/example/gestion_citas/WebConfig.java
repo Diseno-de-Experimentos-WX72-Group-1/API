3d3c991c00daa7f9e69dcd5a69e4e27e030d66d9
Camila Amaro <camilaamarov@yahoo.es>
2024-09-25 18:15:41 -0500

    fix: Fixed problem with CORS

--- src/main/java/com/example/gestion_citas/WebConfig.java
+++ src/main/java/com/example/gestion_citas/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Permite todas las rutas
-                .allowedOriginPatterns("*")
+                .allowedOrigins("https://petcare-fe.web.app","http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
                 .allowedHeaders("*")  // Permite todos los encabezados
                 .allowCredentials(true);  // Permite credenciales
