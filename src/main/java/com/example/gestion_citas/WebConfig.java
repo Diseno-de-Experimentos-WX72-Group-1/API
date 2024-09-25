17aea2a4ffefd631134b3bc85b124d6efe0eb058
Camila Amaro <camilaamarov@yahoo.es>
2024-09-25 13:41:47 -0500

    feat: Added controller, dto and entity files

---
+++ src/main/java/com/example/gestion_citas/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.gestion_citas;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Permite todas las rutas
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+                .allowedHeaders("*")  // Permite todos los encabezados
+                .allowCredentials(true);  // Permite credenciales
+    }
+}
