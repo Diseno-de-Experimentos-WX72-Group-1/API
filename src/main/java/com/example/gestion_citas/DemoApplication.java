ad40f16ac4051663d63e22b0c401a23eeb65d804
Camila Amaro <camilaamarov@yahoo.es>
2024-09-25 22:29:40 -0500

    fix: Fixed problem with files

--- src/main/java/com/example/gestion_citas/DemoApplication.java
+++ src/main/java/com/example/gestion_citas/DemoApplication.java
@@ -3,11 +3,9 @@ package com.example.gestion_citas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.gestion_citas")
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
--- src/main/java/com/example/gestion_citas/WebConfig.java
+++ src/main/java/com/example/gestion_citas/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.gestion_citas;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Permite todas las rutas
-                .allowedOrigins("https://petcare-fe.web.app","http://localhost:8080")
+                .allowedOrigins("vue3000-h5ecfya9hfg7fnbk.canadacentral-01.azurewebsites.net")  // Permite solo tu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
                 .allowedHeaders("*")  // Permite todos los encabezados
                 .allowCredentials(true);  // Permite credenciales
