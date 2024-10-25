1a67c06dc2c25ff365183a2bf7ca24090f7e75dd
Camila Amaro <u202114248@upc.edu.pe>
2024-10-24 21:14:00 -0500

    Delete src directory

--- src/main/java/com/example/gestion_citas/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.gestion_citas.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/gestion_citas/repository/CitaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gestion_citas.repository;
-
-import com.example.gestion_citas.entity.Cita;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CitaRepository extends JpaRepository<Cita, Integer> {
-    List<Cita> findByVeterinario_Id(Integer idVeterinario);
-}
